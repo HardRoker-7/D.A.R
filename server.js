976b03337fedea6c52b7b9195b188b9af1a85948
HardRoker-7 <116948273+HardRoker-7@users.noreply.github.com>
2023-09-06 11:52:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
 // server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const axios = require('axios');
+
+// Import required Node.js modules and libraries
+const express = require('express'); // Import the Express web framework
+const mongoose = require('mongoose'); // Import Mongoose for MongoDB connectivity
+const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const session = require('express-session'); // Middleware for handling sessions
+const passport = require('passport'); // Passport for user authentication
+const LocalStrategy = require('passport-local').Strategy; // Passport's LocalStrategy for username/password authentication
+const bcrypt = require('bcrypt'); // Library for password hashing
+const path = require('path'); // Node.js module for handling file paths
+const app = express(); // Create an Express application
+const PORT = process.env.PORT || 3000; // Define the port number for the server to listen on
+const axios = require('axios'); // Library for making HTTP requests
+
+// Set up the view engine and views directory for rendering HTML templates
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+// Use the 'body-parser' middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up session middleware with a secret key
 app.use(session({
     secret: 'your-secret-key',
     resave: false,
     saveUninitialized: false,
 }));
+
+// Initialize Passport middleware for handling authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Connect to MongoDB online version using the link ans stockApp is the name of the database
+// Connect to an online MongoDB instance named 'stockApp'
 mongoose.connect('mongodb+srv://project:project@cluster0.kos1k7l.mongodb.net/stockApp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-// this is where the data type declaration
-//  is done  for my database
+
+// Define the data schema for the 'Stock' and 'User' collections in MongoDB
 const stockSchema = new mongoose.Schema({
     symbol: String,
     qty: Number,
@@ -43,15 +55,16 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-// function for validating username and password
-
+// Method for validating user passwords using bcrypt
 userSchema.methods.validatePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
+// Create models for the 'Stock' and 'User' collections
 const Stock = mongoose.model('Stock', stockSchema);
 const User = mongoose.model('User', userSchema);
 
+// Configure Passport to use the LocalStrategy for user authentication
 passport.use(new LocalStrategy(
     async (username, password, done) => {
         try {
@@ -69,6 +82,7 @@ passport.use(new LocalStrategy(
     }
 ));
 
+// Serialize and deserialize user objects for session management
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
@@ -82,8 +96,7 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
-// function logic to get stock data using axios
-
+// Define a route for handling the root URL ('/')
 app.get('/', ensureAuthenticated, async (req, res) => {
     try {
         const stocks = await Stock.find({ user: req.user._id });
@@ -111,7 +124,7 @@ app.get('/', ensureAuthenticated, async (req, res) => {
 
         const updatedStocks = await Promise.all(stockPromises);
 
-        // shows the index page with the stock data requested by user if there is no error
+        // Render the 'index' template with the stock data requested by the user
         res.render('index', { stocks: updatedStocks });
 
     } catch (err) {
@@ -119,14 +132,19 @@ app.get('/', ensureAuthenticated, async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+// ... (Continuing from the previous code)
+
+// Define another route for handling the root URL ('/') - this seems to be a duplicate
+// It should be noted that only one of these route handlers will be executed since they both handle the same URL.
 app.get('/', ensureAuthenticated, async (req, res) => {
     try {
         const stocks = await Stock.find({ user: req.user._id });
 
-        // Calculate chart data nad creates chart donut based on the data like symbol and sum of qty vs price
+        // Calculate chart data and create a chart (not included in the code, likely handled by a front-end library)
         const chartLabels = stocks.map(stock => stock.symbol);
         const chartData = stocks.map(stock => stock.qty * stock.symbolValue);
 
+        // Render the 'index' template with additional chart data
         res.render('index', { stocks, chartLabels, chartData });
     } catch (err) {
         console.error(err);
@@ -134,9 +152,7 @@ app.get('/', ensureAuthenticated, async (req, res) => {
     }
 });
 
-
-// function to limit the number of stocks users can add to 5 to limit api usage
-
+// Define a route for adding stocks to a user's portfolio
 app.post('/addStock', ensureAuthenticated, async (req, res) => {
     const { symbol, qty } = req.body;
     try {
@@ -145,24 +161,25 @@ app.post('/addStock', ensureAuthenticated, async (req, res) => {
             return res.status(400).send('Maximum stock limit reached');
         }
 
-
+        // Create a new stock record associated with the current user
         await Stock.create({ symbol, qty, user: req.user._id });
 
+        // Redirect the user back to the root URL ('/')
         res.redirect('/');
-
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
     }
 });
 
-// function logic to delete stocks
-
-
+// Define a route for deleting stocks from a user's portfolio
 app.post('/deleteStock', ensureAuthenticated, async (req, res) => {
     const { id } = req.body;
     try {
+        // Find and delete a stock record by its ID
         await Stock.findByIdAndDelete(id);
+
+        // Redirect the user back to the root URL ('/')
         res.redirect('/');
     } catch (err) {
         console.error(err);
@@ -170,115 +187,161 @@ app.post('/deleteStock', ensureAuthenticated, async (req, res) => {
     }
 });
 
-// login,register and logout logic and routes using passport.LocalStrategy
-
+// Define a route for handling user login attempts
 app.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
 }));
 
+// Define a route for displaying the login form
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
+// Define a route for displaying the registration form
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
+// Define a route for handling user registration
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        // bcrypt is a package which converts users paswword into code so that humans can identiy
-        //  them and the 10 is delay added for security
+        // Hash the user's password for secure storage
         const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user record with the hashed password
         await User.create({ username, password: hashedPassword });
+
+        // Redirect the user to the login page after successful registration
         res.redirect('/login');
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
     }
 });
+// Define a route for user logout
 app.get('/logout', function (req, res, next) {
+    // Call the 'req.logout' function, provided by Passport, to log the user out
     req.logout(function (err) {
-        if (err) { return next(err); }
-        res.redirect('/');
+        if (err) { return next(err); } // Handle any potential errors during logout
+        res.redirect('/'); // Redirect the user to the root URL ('/') after successful logout
     });
 });
+
+// Middleware function to ensure that a user is authenticated before accessing certain routes
 function ensureAuthenticated(req, res, next) {
+    // Check if the user is authenticated using Passport's 'req.isAuthenticated' function
     if (req.isAuthenticated()) {
-        return next();
+        return next(); // If authenticated, allow the user to access the next middleware or route
     }
+    // If not authenticated, redirect the user to the login page
     res.redirect('/login');
 }
 
+// Define a route for fetching a user's stock portfolio
 app.get('/stocks', async (req, res) => {
     try {
         if (!req.session.user) {
+            // Check if there is no user session, indicating the user is not logged in
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
+        // Retrieve the user object from the session
         const user = req.session.user;
 
+        // Find and retrieve stocks associated with the user's ID from the database
         const stocks = await Stock.find({ user: user._id });
+
+        // Respond with the retrieved stock data in JSON format
         res.json(stocks);
     } catch (err) {
         console.error('Error fetching stock data:', err);
+        // Handle any errors during stock retrieval and respond with an error message
         res.status(500).json({ error: 'Error fetching stock data.' });
     }
 });
 
+// Define another route for fetching all stocks (potentially for a different purpose)
 app.get('/stocks', async (req, res) => {
     try {
+        // Retrieve all stocks from the database
         const stocks = await Stock.find();
+
+        // Respond with the retrieved stock data in JSON format
         res.json(stocks);
     } catch (err) {
         console.error('Error fetching stock data:', err);
+        // Handle any errors during stock retrieval and respond with an error message
         res.status(500).json({ error: 'Error fetching stock data.' });
     }
-});
-// Routes
+});// Define various routes for different URLs
+
+// Route for '/index' redirects to the root URL ('/')
 app.get('/index', (req, res) => {
     res.redirect('/');
 });
+
+// Route for '/api' serves an HTML file located in the 'views' directory
 app.get('/api', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'api.html');
     res.sendFile(filePath);
 });
+
+// Route for '/news' serves an HTML file located in the 'views' directory
 app.get('/news', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'news.html');
     res.sendFile(filePath);
 });
+
+// Route for '/sitemap' serves an HTML file located in the 'views' directory
 app.get('/sitemap', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'sitemap.html');
     res.sendFile(filePath);
 });
+
+// Route for '/asian' serves an HTML file located in the 'views' directory
 app.get('/asian', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'asian.html');
     res.sendFile(filePath);
 });
+
+// Route for '/land' serves an HTML file located in the 'views' directory
 app.get('/land', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'landing.html');
     res.sendFile(filePath);
 });
+
+// Route for '/video' serves an HTML file located in the 'views' directory
 app.get('/video', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'video.html');
     res.sendFile(filePath);
 });
+
+// Route for '/logout' destroys the user session and redirects to the root URL ('/')
 app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
+    req.session.destroy(); // Destroy the user's session data
+    res.redirect('/'); // Redirect the user to the root URL
 });
+
+// Middleware for serving JavaScript files under the '/js' path with the correct content type
 app.use('/js', (req, res, next) => {
     res.setHeader('Content-Type', 'application/javascript');
     next();
 });
+
+// Serve static JavaScript files from the 'public/js' directory
 app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
+
+// Serve static assets from the 'assets' directory
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Route for '/video' renders an HTML template named 'video'
 app.get('/video', (req, res) => {
     res.render('video');
 });
 
-
+// Start the server and listen on the specified port (PORT)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
